6cd8081b119e0315d475510575869fdfbabe5f83
biiont <biiont@ixtent.net>
2015-07-06 19:40:25 +0300

    Implemented checksum fix mode and made it running by default.

--- nmeacksum.py
+++ nmeacksum.py
@@ -4,7 +4,6 @@
 ### TODO: Add option to read checksums from multiple files.
 ### TODO: Add mode to check one sentence (either cmdline or stdin or file) and return appropriate error code.
 ### TODO: Add mode to filter NMEA sentences by their checksum.
-### TODO: Add mode to fix NMEA checksums (to work in pipe mode mostly, probably this mode should be used by default).
 
 from __future__ import generators
 
@@ -13,20 +12,24 @@ from functools import reduce
 from operator import xor
 from sys import stdin
 
-def strip_nmea_sentence(sentence) :
-    """ Strip NMEA sentence from '$', '*' and checksum."""
-    asterisk_idx = sentence.rfind('*')
-    return sentence[0:asterisk_idx].lstrip('$')
-
-def calculate_nmea_checksum(data) :
+def checksum_xor_8bit(data) :
     """ Calculates checksum XORing all passed symbols.
     
     Sutable for checksumming NMEA sentences. In this case pass NMEA sentence string without '$' and '*hh<CR><LF>' parts.
     """
     return reduce(lambda a, b: a ^ b, (ord(c) for c in data))
 
+def strip_nmea_sentence(sentence) :
+    """ Strip NMEA sentence from '$', '*' and checksum."""
+    asterisk_idx = sentence.rfind('*')
+    return sentence[0:asterisk_idx].lstrip('$')
+
+def calculate_nmea_checksum(sentence) :
+    return checksum_xor_8bit(strip_nmea_sentence(sentence))
+
 def fix_nmea_checksum(sentence) :
-    return None
+    sentence = strip_nmea_sentence(sentence)
+    return '${}*{:02X}\r\n'.format(sentence, checksum_xor_8bit(sentence))
 
 if __name__ == '__main__' :
     parser = argparse.ArgumentParser(description='Calculate checksum for NMEA sentence.')
@@ -37,11 +40,8 @@ if __name__ == '__main__' :
 
     sentences = args.sentences if len(args.sentences) > 0 else (line for line in stdin)
 
-    #print(list(calculate_nmea_checksum(sent) for sent in sentences))
     try:
-        for line in ( (strip_nmea_sentence(sent),
-                       hex(calculate_nmea_checksum(strip_nmea_sentence(sent))))
-                      for sent in sentences):
-            print(line)
+        for sentence in sentences :
+            print(fix_nmea_checksum(sentence))
     except (KeyboardInterrupt, SystemExit):
         pass
