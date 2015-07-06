8349b37c8307fed41133a2406b30ad86cbe5587a
biiont <biiont@ixtent.net>
2015-07-06 19:11:20 +0300

    Add NMEA-0183 sentences checksum checker prototype.

---
+++ nmeacksum.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from __future__ import generators
+
+import argparse
+from functools import reduce
+from operator import xor
+from sys import stdin
+
+def strip_nmea_sentence(sentence) :
+    """ Strip NMEA sentence from '$', '*' and checksum."""
+    asterisk_idx = sentence.rfind('*')
+    return sentence[0:asterisk_idx].lstrip('$')
+
+def calculate_nmea_checksum(data) :
+    """ Calculates checksum XORing all passed symbols.
+    
+    Sutable for checksumming NMEA sentences. In this case pass NMEA sentence string without '$' and '*hh<CR><LF>' parts.
+    """
+    return reduce(lambda a, b: a ^ b, (ord(c) for c in data))
+
+def fix_nmea_checksum(sentence) :
+    return None
+
+if __name__ == '__main__' :
+    parser = argparse.ArgumentParser(description='Calculate checksum for NMEA sentence.')
+    parser.add_argument('--version', action='version', version='0.1')
+    parser.add_argument('sentences', help='Quoted NMEA sentences with or without <CR><LF>, "$", "*".',
+                        metavar="'NMEA'", nargs='*')
+    args = parser.parse_args()
+
+    sentences = args.sentences if len(args.sentences) > 0 else (line for line in stdin)
+
+    #print(list(calculate_nmea_checksum(sent) for sent in sentences))
+    try:
+        for line in ( (strip_nmea_sentence(sent),
+                       hex(calculate_nmea_checksum(strip_nmea_sentence(sent))))
+                      for sent in sentences):
+            print(line)
+    except (KeyboardInterrupt, SystemExit):
+        pass
